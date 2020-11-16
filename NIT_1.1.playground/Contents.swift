b4d8529acab47d2dcb53b74e2da9e0c34233307f
RoSpr <rsprikut@mail.ru>
2020-11-17 00:10:31 +0500

    Changed NIT_1.1.playground

--- NIT_1.1.playground/Contents.swift
+++ NIT_1.1.playground/Contents.swift
@@ -1,11 +1,23 @@
 import UIKit
 
 class Figure {
-    var name: String = "Some Figure"
-    var numberOfCorners: Int = 0
+    var name: String {
+        get { return "Some Figure" }
+    }
+    var numberOfCorners: Int {
+        get { return 0 }
+    }
 }
 
 class Triangle: Figure {
+    override var name: String {
+        get { return "Triangle" }
+    }
+    
+    override var numberOfCorners: Int {
+        get { return 3 }
+    }
+    
     func countArea(height: Double, side_size: Double) -> Double {
         let area = 0.5 * height * side_size
         return area
@@ -13,15 +25,31 @@ class Triangle: Figure {
 }
 
 class Circle: Figure {
+    override var name: String {
+        get { return "Circle" }
+    }
+    
+    override var numberOfCorners: Int {
+        get { return 0 }
+    }
+    
     func countArea(radius: Double) -> Double {
         let area = Double.pi * pow(radius, 2)
         return area
     }
 }
 
-class Rectangle: Figure, equalOppositeSides {
+class Rectangle: Figure, EqualOppositeSides {
     var height: Double = 0.0
     var width: Double = 0.0
+    
+    override var name: String {
+        get { return "Rectangle" }
+    }
+    
+    override var numberOfCorners: Int {
+        get { return 4 }
+    }
     
     func countArea() -> Double {
         let area = height * width
@@ -30,7 +58,7 @@ class Rectangle: Figure, equalOppositeSides {
 }
 
 
-protocol equalOppositeSides {
+protocol EqualOppositeSides {
     var height: Double { get }
     var width: Double { get }
     var perimeter: Double { get }
